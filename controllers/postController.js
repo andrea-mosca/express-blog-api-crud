5cdb76555c090e4730208d892290db6966ebb43e
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-14 16:01:53 +0200

    milestone 2

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,7 +25,15 @@ function update(req, res) {
 };
 
 function destroy(req, res) {
-    res.send("Eliminazione del post " + req.params.id);
+    const id = parseInt(req.params.id);
+    const post = posts.find(currentPost => currentPost.id === id);
+    const postIndex = posts.indexOf(post);
+    if (postIndex === -1) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+    posts.splice(postIndex, 1);
+    console.log(posts);
+    res.sendStatus(204);
 };
 
 module.exports = {index, show, create, update, destroy};
